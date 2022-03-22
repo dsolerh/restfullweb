a5d773ef745e264b4f92d17657bd640a199d254c
dsolerh <dsolerh.cinter95@gmail.com>
2022-03-21 22:17:46 -0400

    add mirrorFinder

---
+++ chapter1/mirrorFinder/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/dsolerh/restfullweb/chapter1/mirrors"
+)
+
+type response struct {
+	FastestURL string        `json:fastest_url`
+	Latency    time.Duration `json:latency`
+}
+
+func main() {
+	http.HandleFunc("/fastest-mirror", func(w http.ResponseWriter, r *http.Request) {
+		response := findFastest(mirrors.MirrorList)
+		respJSON, _ := json.Marshal(response)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(respJSON)
+	})
+	addr := ":8000"
+	server := &http.Server{
+		Addr:           addr,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	fmt.Printf("Starting server on address %s\n", addr)
+	log.Fatal(server.ListenAndServe())
+}
+
+func findFastest(urls []string) response {
+	urlChan := make(chan string)
+	latencyChan := make(chan time.Duration)
+
+	for _, url := range urls {
+		mirrorURL := url
+		go func() {
+			start := time.Now()
+			_, err := http.Get(mirrorURL + "/README")
+			latency := time.Since(start) / time.Millisecond
+			if err == nil {
+				urlChan <- mirrorURL
+				latencyChan <- latency
+			}
+		}()
+	}
+	return response{<-urlChan, <-latencyChan}
+}
