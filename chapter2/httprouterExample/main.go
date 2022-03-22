a348a2121a747e8e63135a5c280abb48974827f5
dsolerh <dsolerh.cinter95@gmail.com>
2022-03-22 18:24:28 -0400

    httpRouterExample

---
+++ chapter2/httprouterExample/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os/exec"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	router := httprouter.New()
+	router.GET("/api/v1/go-version", goVersion)
+	router.GET("/api/v1/show-file/:name", getFileContent)
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func getCommandOutput(command string, arguments ...string) string {
+	out, err := exec.Command(command, arguments...).Output()
+	if err != nil {
+		log.Println(err)
+	}
+	return string(out)
+}
+
+func goVersion(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	response := getCommandOutput("/usr/local/go/bin/go", "version")
+	fmt.Fprintln(w, response)
+}
+
+func getFileContent(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	fmt.Fprintln(w, getCommandOutput("/bin/cat", params.ByName("name")))
+}
