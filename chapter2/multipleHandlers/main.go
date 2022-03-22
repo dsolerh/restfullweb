e34a7431c79376e390b816187f696627ab00d517
dsolerh <dsolerh.cinter95@gmail.com>
2022-03-22 18:24:13 -0400

    create multipleHandlers

---
+++ chapter2/multipleHandlers/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/randomFloat", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, rand.Float64())
+	})
+	mux.HandleFunc("/randomInt", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, rand.Intn(100))
+	})
+	http.ListenAndServe(":8000", mux)
+}
